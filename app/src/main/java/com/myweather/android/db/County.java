6bf1fb406c853dafeb7f3f4f1e721c1deece32b9
junker <13030231516@126.com>
2018-10-09 15:45:57 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/myweather/android/db/County.java
@@ -0,0 +1,46 @@
+package com.myweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int ciryId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCiryId() {
+        return ciryId;
+    }
+
+    public void setCiryId(int ciryId) {
+        this.ciryId = ciryId;
+    }
+}
